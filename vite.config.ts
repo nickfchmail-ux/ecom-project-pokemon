15002f0b5bae2e74b75c7ffc14748791db48fe8d
nick <nickfchmail@gmail.com>
2025-12-01 21:00:14 +0800

    deployment set up

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 // vite.config.ts   ← must be in the root!
-import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path"; // ← this line is crucial
+import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint2";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), eslint(), tailwindcss()],
+  build: { outDir: "build" },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // ← THIS is what fixes your error
