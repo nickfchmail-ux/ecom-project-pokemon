0068cbd43ef6c90154c9706d08edf8ea5f3adb5c
nick <nickfchmail@gmail.com>
2025-12-02 09:02:25 +0800

    nav styling

--- src/features/productCard/Product.jsx
+++ src/features/productCard/Product.jsx
@@ -19,7 +19,6 @@ function Product() {
       <div className="mx-auto grid w-[75vw] grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         <Card products={pokemons} loading={isLoading} />
       </div>
-      ;
     </>
   );
 }
