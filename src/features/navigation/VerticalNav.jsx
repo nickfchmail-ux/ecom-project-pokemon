ed4ef544da17533d90fd1b80dd96be914b8c5439
nick <nickfchmail@gmail.com>
2025-12-02 08:56:21 +0800

    nav styling

--- src/features/navigation/VerticalNav.jsx
+++ src/features/navigation/VerticalNav.jsx
@@ -1,3 +1,4 @@
+import Logo from "@/ui/Logo";
 import { useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
@@ -25,21 +26,51 @@ export function VerticalNav({ checked, setChecked }) {
     >
       {" "}
       <div className="relative p-6" ref={ref}>
-        <div className="mb-8 text-2xl font-bold">My Logo</div>
-        <nav className="flex flex-col gap-6 text-lg">
-          <NavLink to="/" onClick={() => setChecked(!checked)}>
+        <Logo />
+        <nav className="mt-3 flex flex-col items-center justify-center gap-6 text-lg">
+          <NavLink
+            to="/"
+            onClick={() => setChecked(!checked)}
+            className={
+              "rounded-xl px-4 py-2 font-semibold text-amber-500 shadow-lg shadow-yellow-500/50 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl hover:shadow-amber-500/60 active:translate-y-0 active:shadow-amber-500/40"
+            }
+          >
             Home
           </NavLink>
-          <NavLink to="/product" onClick={() => setChecked(!checked)}>
+          <NavLink
+            to="/product"
+            onClick={() => setChecked(!checked)}
+            className={
+              "rounded-xl px-4 py-2 font-semibold text-emerald-500 shadow-lg shadow-emerald-500/50 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl hover:shadow-emerald-500/60 active:translate-y-0 active:shadow-emerald-500/40"
+            }
+          >
             Product
           </NavLink>
-          <NavLink to="/cart" onClick={() => setChecked(!checked)}>
+          <NavLink
+            to="/cart"
+            onClick={() => setChecked(!checked)}
+            className={
+              "rounded-xl px-4 py-2 font-semibold text-cyan-500 shadow-lg shadow-cyan-500/50 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl hover:shadow-cyan-500/60 active:translate-y-0 active:shadow-cyan-500/40"
+            }
+          >
             Cart
           </NavLink>
-          <NavLink to="/contact" onClick={() => setChecked(!checked)}>
+          <NavLink
+            to="/contact"
+            onClick={() => setChecked(!checked)}
+            className={
+              "rounded-xl px-4 py-2 font-semibold text-lime-500 shadow-lg shadow-lime-500/50 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl hover:shadow-lime-500/60 active:translate-y-0 active:shadow-lime-500/40"
+            }
+          >
             Contact
           </NavLink>
-          <NavLink to="/table" onClick={() => setChecked(!checked)}>
+          <NavLink
+            to="/table"
+            onClick={() => setChecked(!checked)}
+            className={
+              "rounded-xl px-4 py-2 font-semibold text-fuchsia-500 shadow-lg shadow-fuchsia-500/50 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl hover:shadow-fuchsia-500/60 active:translate-y-0 active:shadow-fuchsia-500/40"
+            }
+          >
             Table
           </NavLink>
         </nav>
